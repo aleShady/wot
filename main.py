f1f3c5dd7a9ebc5d3383b3049e13fd0ec1bd6d21
aleShady <siverinoa@gmail.com>
2021-05-25 22:08:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-from telegram.ext import Updater
-updater = Updater(token='TOKEN', use_context=True)
-
-dispatcher = updater.dispatcher
-
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")    
-
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-
-updater.start_polling()
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
